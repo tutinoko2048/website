ad2dfa005e7568b86441e0fbd1e189a76518e199
RetoRuto9900K <tutinoko2048@gmail.com>
2026-04-03 20:19:54 +0900

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  redirects: {
+    '/': {
+      status: 302, // temporary redirect
+      destination: 'https://github.com/tutinoko2048'
+    }
+  }
+});
